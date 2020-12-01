ee18375fcbd09a4dc13753a8f1c33d60b7236b27
wachira_Dev <ewachira254@gmail.com>
2020-12-01 13:05:19 +0300

    ⭐ @day1 part two is done

--- 2020/day1/index.js
+++ 2020/day1/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-function checkNumbers() {
+function checkNumbersPartOne() {
   const arrayOfNumbers = fs
     .readFileSync("input.txt", { encoding: "utf8" })
     .split("\n")
@@ -14,12 +14,12 @@ function checkNumbers() {
     firstAddend++
   ) {
     for (
-      let secondAdded = firstAddend + 1;
-      secondAdded < arrayOfNumbers.length;
-      secondAdded++
+      let secondAddend = firstAddend + 1;
+      secondAddend < arrayOfNumbers.length;
+      secondAddend++
     ) {
-      if (arrayOfNumbers[firstAddend] + arrayOfNumbers[secondAdded] === 2020) {
-        result = arrayOfNumbers[firstAddend] * arrayOfNumbers[secondAdded];
+      if (arrayOfNumbers[firstAddend] + arrayOfNumbers[secondAddend] === 2020) {
+        result = arrayOfNumbers[firstAddend] * arrayOfNumbers[secondAddend];
 
         break;
       }
@@ -29,4 +29,48 @@ function checkNumbers() {
   return result;
 }
 
-console.log(checkNumbers());
+function checkNumbersPartTwo() {
+  const arrayOfNumbers = fs
+    .readFileSync("input.txt", { encoding: "utf8" })
+    .split("\n")
+    .map((x) => parseInt(x, 10));
+
+  let result = 0;
+
+  for (
+    let firstAddend = 0;
+    firstAddend < arrayOfNumbers.length;
+    firstAddend++
+  ) {
+    for (
+      let secondAddend = firstAddend + 1;
+      secondAddend < arrayOfNumbers.length;
+      secondAddend++
+    ) {
+      for (
+        let thirdAddend = secondAddend + 1;
+        thirdAddend < arrayOfNumbers.length;
+        thirdAddend++
+      ) {
+        if (
+          arrayOfNumbers[firstAddend] +
+            arrayOfNumbers[secondAddend] +
+            arrayOfNumbers[thirdAddend] ===
+          2020
+        ) {
+          result =
+            arrayOfNumbers[firstAddend] *
+            arrayOfNumbers[secondAddend] *
+            arrayOfNumbers[thirdAddend];
+
+          break;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(checkNumbersPartOne());
+console.log(checkNumbersPartTwo());
